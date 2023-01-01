50d58a9a3f1096eb08001c22ec8dc24f8019921b
Lemon Rose <yash.kul69@gmail.com>
2023-01-02 01:10:58 +0530

    initiate web view app with proper error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+extern crate wry;
+
+use wry::{
+    application::{
+        event::{Event, StartCause, WindowEvent},
+        event_loop::{ControlFlow, EventLoop, self},
+        window::WindowBuilder,
+    },
+    webview::WebViewBuilder,
+};
+
+const DISCORD: &str = "https://discord.com/app";
+
+fn main() -> wry::Result<()> {
+    let event_loop = EventLoop::new();
+
+    let window = WindowBuilder::new()
+        .with_title("LemonCord")
+        .build(&event_loop)?;
+
+    let _webview = WebViewBuilder::new(window)?
+        .with_url(DISCORD)?
+        .build()?;
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::NewEvents(StartCause::Init) => println!("Discord web view successfully started."),
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
 }
