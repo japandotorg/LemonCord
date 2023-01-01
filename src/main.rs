eb1c9e63782d336ab91140c24a8bb0325157c0db
Lemon Rose <yash.kul69@gmail.com>
2023-01-02 05:02:31 +0530

    user agent, handling and more

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,50 @@
 extern crate wry;
+extern crate anyhow;
 
+use anyhow::Result;
 use wry::{
     application::{
         event::{Event, StartCause, WindowEvent},
         event_loop::{ControlFlow, EventLoop},
         window::WindowBuilder,
     },
-    webview::WebViewBuilder,
+    webview::{webview_version, WebViewBuilder},
 };
 
-const DISCORD: &str = "https://discord.com/app";
+pub const DISCORD: &str = "https://discord.com/app";
+pub const APP_NAME: &str = "LemonCord";
 
-fn main() -> wry::Result<()> {
+fn main() -> Result<()> {
+    let version_info = env!("CARGO_PKG_VERSION");
+
+    let webview_version_info = webview_version().unwrap();
+
+    let user_agent = format!(
+        "{} v{} ({}; {})",
+        APP_NAME,
+        version_info,
+        std::env::consts::OS,
+        webview_version_info
+    );
+
     let event_loop = EventLoop::new();
 
     let window = WindowBuilder::new()
-        .with_title("LemonCord")
-        .build(&event_loop)?;
+        .with_title(APP_NAME)
+        .with_transparent(true)
+        .with_resizable(true)
+        .build(&event_loop)
+        .unwrap_or_else(
+            |_| 
+                panic!(
+                    "Unable to build window!"
+                )
+        );
 
-    let _webview = WebViewBuilder::new(window)?
+    #[allow(unused_mut)]
+    let mut _webview = WebViewBuilder::new(window)?
+        .with_user_agent(&user_agent)
+        .with_devtools(cfg!(any(debug_assertions, feature = "devtools")))
         .with_url(DISCORD)?
         .build()?;
 
