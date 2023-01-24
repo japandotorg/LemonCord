d9ffcada761942dede4c1849a114e49fb7dda7d6
Lemon Rose <yash.kul69@gmail.com>
2023-01-24 22:15:17 +0530

    proper macos support

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,22 @@ use wry::{
     application::{
         event::{Event, StartCause, WindowEvent},
         event_loop::{ControlFlow, EventLoop},
-        window::{WindowBuilder, Icon},
+        window::{WindowBuilder, Window, Icon}, 
+        menu::MenuType,
     },
     webview::WebViewBuilder,
 };
 
+#[cfg(target_os = "macos")]
+use wry::application::{
+    accelerator::{Accelerator, SysMods},
+    keyboard::KeyCode,
+    menu::{MenuBar as Menu, MenuItem, MenuItemAttributes},
+    platform::macos::WindowBuilderExtMacOS,
+};
+
 #[cfg(target_os = "windows")]
 use wry::application::platform::windows::WindowBuilderExtWindows;
-
-#[cfg(target_os = "macos")]
-use wry::application::platform::macos::WindowBuilderExtMacOS;
 
 #[cfg(any(target_os = "linux", target_os = "windows"))]
 use wry::webview::WebContext;
@@ -38,6 +44,30 @@ pub const APP_NAME: &str = "LemonCord";
 
 
 fn main() -> Result<()> {
+    #[cfg(target_os = "macos")]
+    let (menu_bar_menu, close_item) = {
+        let mut menu_bar_menu = Menu::new();
+        let mut first_menu = Menu::new();
+        first_menu.add_native_item(MenuItem::Hide);
+        first_menu.add_native_item(MenuItem::EnterFullScreen);
+        first_menu.add_native_item(MenuItem::Minimize);
+        first_menu.add_native_item(MenuItem::Separator);
+        first_menu.add_native_item(MenuItem::Copy);
+        first_menu.add_native_item(MenuItem::Cut);
+        first_menu.add_native_item(MenuItem::Paste);
+        first_menu.add_native_item(MenuItem::Undo);
+        first_menu.add_native_item(MenuItem::Redo);
+        first_menu.add_native_item(MenuItem::SelectAll);
+        first_menu.add_native_item(MenuItem::Separator);
+        let close_item = first_menu.add_item(
+            MenuItemAttributes::new("CloseWindow")
+                .with_accelerators(&Accelerator::new(SysMods::Cmd, KeyCode::KeyW)),
+        );
+        first_menu.add_native_item(MenuItem::Quit);
+        menu_bar_menu.add_submenu("App", true, first_menu);
+        (menu_bar_menu, close_item)
+    };
+
     // Most common User-Agent.
     let user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36".to_string();
 
@@ -98,6 +128,7 @@ fn main() -> Result<()> {
             .with_titlebar_buttons_hidden(false)
             .with_titlebar_transparent(true)
             .with_title_hidden(false)
+            .with_menu(menu_bar_menu)
             .build(&event_loop)
             .unwrap_or_else(
                 |_|
@@ -105,6 +136,15 @@ fn main() -> Result<()> {
                         "Unable to build window!"
                     )
             )
+    };
+
+    let _handler = move |window: &Window, req: String| {
+        if req == "drag_window" {
+            let _ = window.drag_window();
+        } else if req == "fullscreen" {
+            let is_maximuzed = window.is_maximized();
+            window.set_maximized(!is_maximuzed);
+        }
     };
 
     #[cfg(any(target_os = "linux", target_os = "windows"))]
@@ -144,6 +184,18 @@ fn main() -> Result<()> {
             .build()?,
     );
 
+    #[allow(unused_mut)]
+    #[cfg(target_os = "macos")]
+    let _webview = {
+        WebViewBuilder::new(window)?
+            .with_user_agent(&user_agent)
+            .with_url(&DISCORD.to_string())?
+            .with_devtools(cfg!(any(debug_assertions, feature = "devtools")))
+            .with_ipc_handler(_handler)
+            .with_back_forward_navigation_gestures(true)
+            .build()?
+    };
+
     #[cfg(feature = "devtools")]
     {
         _webview.open_devtools();
@@ -161,6 +213,17 @@ fn main() -> Result<()> {
             | Event::UserEvent(UserEvents::CloseWindow) => {
                 let _ = _webview.take();
                 *control_flow = ControlFlow::Exit
+            }
+            | Event::MenuEvent {
+                menu_id,
+                origin: MenuType::MenuBar,
+                ..
+            } => {
+                #[cfg(target_os = "macos")]
+                if menu_id == close_item.clone().id() {
+                    window.set_minimized(true)
+                }
+                println!("Clicked on {:?}", menu_id);
             }
             _ => (),
         }
