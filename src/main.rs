a270514fb26c217dd2ceecac27c826b7bc20d788
Lemon Rose <yash.kul69@gmail.com>
2023-01-24 20:55:08 +0530

    proper cross-platform support, context, img loader

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 extern crate wry;
+extern crate dirs;
 extern crate image;
 extern crate anyhow;
+
 
 use wry::{
     application::{
         event::{Event, StartCause, WindowEvent},
         event_loop::{ControlFlow, EventLoop},
-        window::{WindowBuilder, Icon}
+        window::{WindowBuilder, Icon},
     },
     webview::WebViewBuilder,
 };
+
+#[cfg(target_os = "windows")]
+use wry::application::platform::windows::WindowBuilderExtWindows;
+
+#[cfg(target_os = "macos")]
+use wry::application::platform::macos::WindowBuilderExtMacOS;
+
+#[cfg(any(target_os = "linux", target_os = "windows"))]
+use wry::webview::WebContext;
 
 
 #[allow(dead_code)]
@@ -27,43 +38,116 @@ pub const APP_NAME: &str = "LemonCord";
 
 
 fn main() -> Result<()> {
-    #[allow(unused_mut)]
-    let mut icon = image::open("assets/logo.webp")
-        .expect("Failed to open icon path.")
-        .to_rgba8();
-
-    let (icon_width, icon_height) = icon.dimensions();
-
     // Most common User-Agent.
     let user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36".to_string();
 
     let event_loop = EventLoop::<UserEvents>::with_user_event();
 
-    let window = WindowBuilder::new()
+    let main_window = WindowBuilder::new()
         .with_title(APP_NAME)
-        .with_window_icon(Some(
-            Icon::from_rgba(icon.clone().into_raw(), icon_width, icon_height).unwrap(),
-        ))
         .with_transparent(true)
-        .with_resizable(true)
-        .build(&event_loop)
-        .unwrap_or_else(
-            |_|
-                panic!(
-                    "Unable to build window!"
-                )
-        );
+        .with_resizable(true);
+
+    #[cfg(target_os = "windows")]
+    let window = {
+        let mut icon_path = format!("assets/{}_logo.webp", APP_NAME);
+
+        if !std::path::Path::new(&icon_path).exists() {
+            icon_path = "assets/logo.webp".to_string();
+        }
+
+        let icon = load_icon(std::path::Path::new(&icon_path));
+
+        main_window
+            .with_window_icon(Some(icon.clone()))
+            .with_taskbar_icon(Some(icon.clone()))
+            .build(&event_loop)
+            .unwrap_or_else(
+                |_|
+                    panic!(
+                        "Unable to build window!"
+                    )
+            )
+    };
+
+    #[cfg(target_os = "linux")]
+    let window = {
+        let mut icon_path = format!("assets/{}_logo.webp", APP_NAME);
+
+        if !std::path::Path::new(&icon_path).exists() {
+            icon_path = "assets/logo.webp".to_string();
+        }
+
+        let icon = load_icon(std::path::Path::new(&icon_path));
+
+        main_window
+            .with_window_icon(Some(icon.clone()))
+            .build(&event_loop)
+            .unwrap_or_else(
+                |_|
+                    panic!(
+                        "Unable to build window!"
+                    )
+            )
+    };
+
+    #[cfg(target_os = "macos")]
+    let window = {
+        main_window
+            .with_fullsize_content_view(true)
+            .with_titlebar_buttons_hidden(false)
+            .with_titlebar_transparent(true)
+            .with_title_hidden(false)
+            .build(&event_loop)
+            .unwrap_or_else(
+                |_|
+                    panic!(
+                        "Unable to build window!"
+                    )
+            )
+    };
+
+    #[cfg(any(target_os = "linux", target_os = "windows"))]
+    let home_dir = match dirs::home_dir() {
+        Some(path1) => path1,
+        None => panic!("Error, can't find the home directory!!"),
+    };
+
+    #[cfg(target_os = "windows")]
+    let data_dir = home_dir.join("AppData").join("Roaming").join(APP_NAME);
+    
+    #[cfg(target_os = "linux")]
+    let data_dir = home_dir.join(".config").join(APP_NAME);
+    
+    #[cfg(any(target_os = "linux", target_os = "windows"))]
+    if !data_dir.exists() {
+        std::fs::create_dir(&data_dir)
+            .unwrap_or_else(
+                |_| 
+                    panic!("Can't create dir {}", data_dir.display())
+            );
+    }
+    
+    #[cfg(any(target_os = "linux", target_os = "windows"))]
+    let mut web_context = WebContext::new(Some(data_dir));
 
     #[allow(unused_mut)]
+    #[cfg(any(target_os = "linux", target_os = "windows"))]
     let mut _webview = Some(
         WebViewBuilder::new(window)?
             .with_user_agent(&user_agent)
             .with_accept_first_mouse(true)
             .with_transparent(true)
             .with_devtools(cfg!(any(debug_assertions, feature = "devtools")))
-            .with_url(DISCORD)?
+            .with_url(&DISCORD.to_string())?
+            .with_web_context(&mut web_context)
             .build()?,
     );
+
+    #[cfg(feature = "devtools")]
+    {
+        _webview.open_devtools();
+    }
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
@@ -81,4 +165,18 @@ fn main() -> Result<()> {
             _ => (),
         }
     });
+}
+
+#[cfg(any(target_os = "linux", target_os = "windows"))]
+fn load_icon(path: &std::path::Path) -> Icon {
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::open(path)
+            .expect("Failed to open icon path.")
+            .into_rgba8();
+
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+    Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon.")
 }
