b709aaa481734197c37f528f76650311982ff869
mutexdeveloper <nathanhackett71@gmail.com>
2023-01-30 19:51:13 +0000

    Fixed newlines in logging system.

--- src/log.rs
+++ src/log.rs
@@ -22,19 +22,19 @@ pub enum Priority {
 pub fn write(message: String, priority: Priority) {
     match priority {
         Priority::High => {
-            println!("[ERROR]: {}\n", message.red())
+            println!("[ERROR]: {}", message.red())
         }
 
         Priority::Medium => {
-            println!("[ERROR]: {}\n", message.truecolor(255, 255, 0))
+            println!("[ERROR]: {}", message.truecolor(255, 255, 0))
         }
 
         Priority::Low => {
-            println!("[ERROR]: {}\n", message.truecolor(177, 177, 0) )
+            println!("[ERROR]: {}", message.truecolor(177, 177, 0) )
         }
 
         Priority::Info => {
-            println!("[LOG]: {}\n", message.truecolor(170, 62, 255))
+            println!("[LOG]: {}", message.truecolor(170, 62, 255))
         }
     }
 }
